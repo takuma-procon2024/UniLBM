15ca3c5ab6e2dce250738d13a7e58dc4aade4df7
kara <g22307@kagawa.kosen-ac.jp>
2024-10-07 08:13:23 +0900

    マテリアル設定を変更

---
+++ Packages/jp.kosen-ac.uni-lbm/Runtime/Uni/FanRotator.cs
@@ -0,0 +1,26 @@
+﻿using UniLbm.Lbm.Behaviours;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace UniLbm.Uni
+{
+    public class FanRotator : MonoBehaviour
+    {
+        [SerializeField] private float rotateSpeed = 1.0f;
+
+        private ILbmForceSource _forceSource;
+
+        private void Start()
+        {
+            TryGetComponent(out _forceSource);
+            Assert.IsNotNull(_forceSource);
+        }
+
+        private void Update()
+        {
+            var forceLen = math.length(_forceSource.Force);
+            transform.rotation *= Quaternion.AngleAxis(rotateSpeed * forceLen, transform.forward);
+        }
+    }
+}
