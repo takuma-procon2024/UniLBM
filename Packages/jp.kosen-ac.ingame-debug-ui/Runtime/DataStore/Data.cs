30acaec595692bd5a7e1a68810328f34c1b1ba21
kara <g22307@kagawa.kosen-ac.jp>
2024-10-09 07:00:04 +0900

    インゲームデバッグ用のUIを作成

---
+++ Packages/jp.kosen-ac.ingame-debug-ui/Runtime/DataStore/Data.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Mathematics;
+
+namespace DataStore
+{
+    [Serializable]
+    public class Data
+    {
+        public List<FloatField> floatFields = new();
+        public List<IntField> intFields = new();
+        public List<StringField> stringFields = new();
+        public List<BoolField> boolFields = new();
+        public List<VectorField> vectorFields = new();
+    }
+
+    [Serializable]
+    public class FloatField
+    {
+        public string name;
+        public float value;
+    }
+
+    [Serializable]
+    public class IntField
+    {
+        public string name;
+        public int value;
+    }
+
+    [Serializable]
+    public class StringField
+    {
+        public string name;
+        public string value;
+    }
+
+    [Serializable]
+    public class BoolField
+    {
+        public string name;
+        public bool value;
+    }
+
+    [Serializable]
+    public class VectorField
+    {
+        public string name;
+        public float4 value;
+    }
+}
---
+++ Packages/jp.kosen-ac.ingame-debug-ui/Runtime/UI/MainWindow.cs
@@ -0,0 +1,55 @@
+﻿using LitMotion;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace UI
+{
+    public class MainWindow : MonoBehaviour
+    {
+        [SerializeField] private string dataFilePath = "data.json";
+        [SerializeField] private KeyCode toggleKey = KeyCode.F1;
+        [SerializeField] private float2 defaultPos = 0;
+        [SerializeField] private float2 activePos = 0;
+        private bool _isOpen;
+        private MotionHandle _motionHandle;
+        private RectTransform _rectTransform;
+
+        public DataStore.DataStore DataStore { get; private set; }
+
+        private void Start()
+        {
+            var path = Application.dataPath + "/" + dataFilePath;
+            DataStore = new DataStore.DataStore(path);
+            TryGetComponent(out _rectTransform);
+
+            _rectTransform.anchoredPosition = defaultPos;
+        }
+
+        private void Update()
+        {
+            OpenAndClose();
+        }
+
+        private void OnDestroy()
+        {
+            DataStore.Dispose();
+        }
+
+        private void OpenAndClose()
+        {
+            if (_motionHandle.IsActive()) return;
+            if (!IsPressOpenKey()) return;
+
+            _motionHandle =
+                LMotion.Create(_isOpen ? activePos : defaultPos, _isOpen ? defaultPos : activePos, 0.3f)
+                    .WithEase(Ease.InOutQuad)
+                    .Bind(v => _rectTransform.anchoredPosition = v);
+            _isOpen = !_isOpen;
+        }
+
+        private bool IsPressOpenKey()
+        {
+            return Input.GetKeyDown(toggleKey);
+        }
+    }
+}
