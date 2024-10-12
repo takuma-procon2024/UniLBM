8d6963704c2c799715294e978fc4b97e48fa8824
kara <g22307@kagawa.kosen-ac.jp>
2024-10-12 20:56:56 +0900

    スクリーンサイズに合わせて台形補正UIを調整するように変更

--- Packages/jp.kosen-ac.ingame-debug-ui/Runtime/UI/Homography/HomographyImageUI.cs
+++ Packages/jp.kosen-ac.ingame-debug-ui/Runtime/UI/Homography/HomographyImageUI.cs
@@ -1,19 +1,36 @@
-﻿using Projection;
+﻿using System.Linq;
+using Projection;
 using UI.Parts;
 using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace UI.Homography
 {
-    [RequireComponent(typeof(Image))]
     public class HomographyImageUI : MonoBehaviour
     {
         private static readonly int MatrixPropId = Shader.PropertyToID("_HomographyMatrix");
         private static readonly int InvMatrixPropId = Shader.PropertyToID("_HomographyInvMatrix");
 
-        [SerializeField] private DraggableHandle p00, p01, p10, p11;
+        [SerializeField] private DraggableHandle[] handles;
         [SerializeField] private Material homographyMat;
+        [SerializeField] private int targetScreen;
+
+        private DraggableHandle p00, p01, p10, p11;
+
+        private void Start()
+        {
+            TryGetComponent(out RectTransform rectTransform);
+
+            p00 = handles.First(v => v.PosType == DraggableHandle.Pos.P00);
+            p01 = handles.First(v => v.PosType == DraggableHandle.Pos.P01);
+            p10 = handles.First(v => v.PosType == DraggableHandle.Pos.P10);
+            p11 = handles.First(v => v.PosType == DraggableHandle.Pos.P11);
+
+            var screenRes = Screen.resolutions[targetScreen];
+            var aspect = screenRes.width / (float)screenRes.height;
+
+            rectTransform.sizeDelta = new Vector2(aspect * rectTransform.sizeDelta.y, rectTransform.sizeDelta.y);
+        }
 
         private void Update()
         {
--- Packages/jp.kosen-ac.ingame-debug-ui/Runtime/UI/Parts/DraggableHandle.cs
+++ Packages/jp.kosen-ac.ingame-debug-ui/Runtime/UI/Parts/DraggableHandle.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using UI.Homography;
 using Unity.Mathematics;
 using UnityEditor;
 using UnityEngine;
@@ -11,25 +11,50 @@ namespace UI.Parts
     /// </summary>
     public class DraggableHandle : MonoBehaviour, IDragHandler
     {
-        [SerializeField] private float2 min, max;
-
+        public enum Pos
+        {
+            P00,
+            P01,
+            P10,
+            P11
+        }
+        
+        [SerializeField] private float2 padding;
+        [SerializeField] private Pos defaultPos;
+        
+        private float2 _min, _max;
         private RectTransform _rectTransform;
+        public Pos PosType => defaultPos;
 
         public float2 NormalizedPos => new(
-            (_rectTransform.anchoredPosition.x - min.x) / (max.x - min.x),
-            (_rectTransform.anchoredPosition.y - min.y) / (max.y - min.y)
+            (_rectTransform.anchoredPosition.x - _min.x) / (_max.x - _min.x),
+            (_rectTransform.anchoredPosition.y - _min.y) / (_max.y - _min.y)
         );
-
-        private void OnDrawGizmos()
-        {
-            if (_rectTransform == null) return;
-            Handles.Label(transform.position, $"NormalizedPos: {NormalizedPos}");
-        }
 
         private void Start()
         {
             TryGetComponent(out _rectTransform);
             SetPos(_rectTransform.anchoredPosition);
+
+            var uiRoot = GetComponentInParent<HomographyImageUI>();
+            uiRoot.TryGetComponent(out RectTransform rootRect);
+            _min = -(float2)rootRect.sizeDelta / 2 + padding;
+            _max = (float2)rootRect.sizeDelta / 2 - padding;
+            
+            _rectTransform.anchoredPosition = defaultPos switch
+            {
+                Pos.P00 => _min,
+                Pos.P01 => new float2(_min.x, _max.y),
+                Pos.P10 => new float2(_max.x, _min.y),
+                Pos.P11 => _max,
+                _ => _rectTransform.anchoredPosition
+            };
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (_rectTransform == null) return;
+            Handles.Label(transform.position, NormalizedPos.ToString());
         }
 
         public void OnDrag(PointerEventData eventData)
@@ -41,8 +66,8 @@ namespace UI.Parts
         private void SetPos(in float2 pos)
         {
             _rectTransform.anchoredPosition = new float2(
-                math.clamp(pos.x, min.x, max.x),
-                math.clamp(pos.y, min.y, max.y)
+                math.clamp(pos.x, _min.x, _max.x),
+                math.clamp(pos.y, _min.y, _max.y)
             );
         }
     }
