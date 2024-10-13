ad31501fc8c7e6651b4332f34fe4913b451b2493
keigo <g22307@kagawa.kosen-ac.jp>
2024-10-13 12:04:26 +0900

    タッチパネルでのカメラ操作を実装

--- Packages/jp.kosen-ac.uni-lbm/Runtime/Uni/OrbitCamera.cs
+++ Packages/jp.kosen-ac.uni-lbm/Runtime/Uni/OrbitCamera.cs
@@ -1,4 +1,5 @@
-﻿using Unity.Mathematics;
+﻿using System.Linq;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace UniLbm.Uni
@@ -7,7 +8,11 @@ namespace UniLbm.Uni
     {
         [SerializeField] private Transform target;
         [SerializeField] private float rotateSpeed = 3.0f;
+        [SerializeField] private float touchMoveSpeed = 1.0f;
+        [SerializeField] private float moveSpeed = 0.1f;
+        [SerializeField] private float touchRotateSpeed = 1f;
         [SerializeField] private float scrollSpeed = 10;
+        [SerializeField] private float touchScrollSpeed = 0.1f;
         [SerializeField] private float defaultDistance = 10;
         [SerializeField] private float minDistance = 1;
         [SerializeField] private float maxDistance = 20;
@@ -15,13 +20,20 @@ namespace UniLbm.Uni
         [SerializeField] private float defaultX = 45;
 
         private float _distance, _timer, _x, _y;
+        private Vector3 _initTargetPos;
         private bool _isFirstFrame = true;
+
+        /// <summary>
+        ///     直前の2点間の距離
+        /// </summary>
+        private float _prevTouchDistance;
 
         private void Start()
         {
             _y = defaultY;
             _x = defaultX;
             _distance = defaultDistance;
+            _initTargetPos = target.position;
 
             RotateCamera();
             transform.LookAt(target);
@@ -41,15 +53,84 @@ namespace UniLbm.Uni
                 _distance = math.clamp(_distance, minDistance, maxDistance);
             }
 
-            if (!Input.GetMouseButton(0)) return;
+            if (!Input.GetMouseButton(0) && !Input.GetMouseButton(2))
+            {
+                _prevTouchDistance = 0;
+                return;
+            }
             if (_isFirstFrame)
             {
                 _isFirstFrame = false;
                 return;
             }
 
-            _x += Input.GetAxis("Mouse X") * rotateSpeed;
-            _y -= Input.GetAxis("Mouse Y") * rotateSpeed;
+            // 移動
+            if (Input.GetMouseButton(2))
+            {
+                var screenDelta = Vector2.zero;
+                screenDelta.x -= Input.GetAxis("Mouse X") * moveSpeed;
+                screenDelta.y -= Input.GetAxis("Mouse Y") * moveSpeed;
+
+                var camPos = target.position;
+                camPos += transform.right * screenDelta.x;
+                camPos += transform.up * screenDelta.y;
+
+                target.position = camPos;
+            }
+
+            if (Input.touchCount == 2)
+            {
+                // ピンチイン・アウト
+                var touch0 = Input.GetTouch(0);
+                var touch1 = Input.GetTouch(1);
+                var touchDistance = math.distance(touch0.position, touch1.position);
+                var touchDistanceDiff = _prevTouchDistance - touchDistance;
+
+                if (_prevTouchDistance != 0)
+                {
+                    _distance += touchDistanceDiff * touchScrollSpeed;
+                    _distance = math.clamp(_distance, minDistance, maxDistance);
+                }
+
+                // カメラを動かす
+                var screenPos = Vector2.zero;
+                screenPos.x -= Input.touches[0].deltaPosition.x * touchMoveSpeed;
+                screenPos.y -= Input.touches[0].deltaPosition.y * touchMoveSpeed;
+
+                var camPos = target.position;
+                camPos += transform.right * screenPos.x;
+                camPos += transform.up * screenPos.y;
+                target.position = camPos;
+
+                _prevTouchDistance = touchDistance;
+            }
+            else
+            {
+                _prevTouchDistance = 0;
+                
+                if (Input.touchCount == 1 || Input.GetMouseButton(0))
+                {
+                    // 回転
+                    _x += Input.touches.Any()
+                        ? Input.touches[0].deltaPosition.x * touchRotateSpeed
+                        : Input.GetAxis("Mouse X") * rotateSpeed;
+                    _y -= Input.touches.Any()
+                        ? Input.touches[0].deltaPosition.y * touchRotateSpeed
+                        : Input.GetAxis("Mouse Y") * rotateSpeed;
+                }
+            }
+
+            if (Input.touchCount == 3)
+            {
+                // 位置を初期化
+                _y = defaultY;
+                _x = defaultX;
+                _distance = defaultDistance;
+                target.position = _initTargetPos;
+
+                RotateCamera();
+                transform.LookAt(target);
+            }
         }
 
         private void RotateCamera()
