cfe8a41fd0df0cae0a7852539517b918663529c8
kara <g22307@kagawa.kosen-ac.jp>
2024-10-14 09:05:50 +0900

    ディスプレイ設定用のUnity側UIを作成

---
+++ Packages/jp.kosen-ac.ingame-debug-ui/Runtime/UI/DisplayConfig/DisplayConfigManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI.DisplayConfig
+{
+    public class DisplayConfigManager : MonoBehaviour
+    {
+        [SerializeField] private Camera[] targetCameras;
+        private Dictionary<string, Camera> _camTable;
+
+        public string[] CamNames { get; private set; }
+
+        private void Start()
+        {
+            CamNames = new string[targetCameras.Length];
+            for (var i = 0; i < targetCameras.Length; i++)
+            {
+                CamNames[i] = targetCameras[i].name;
+                _camTable.Add(CamNames[i], targetCameras[i]);
+            }
+        }
+
+        public void SetCameraActive(string camName, bool active)
+        {
+            if (_camTable.TryGetValue(camName, out var value))
+                value.enabled = active;
+        }
+
+        public bool TrySetCameraTargetDisplay(string camName, int display)
+        {
+            var displayCount = Display.displays.Length;
+            if (display < 0 || display >= displayCount)
+                return false;
+            if (_camTable.TryGetValue(camName, out var value))
+                value.targetDisplay = display;
+            return true;
+        }
+    }
+}
---
+++ Packages/jp.kosen-ac.ingame-debug-ui/Runtime/UI/DisplayConfig/DisplayConfigUI.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace UI.DisplayConfig
+{
+    public class DisplayConfigUI : MonoBehaviour
+    {
+        
+    }
+}
