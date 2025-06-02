a0b712d1524b40f96b5bf64c7d60acbbfe86dd29
g22307 <g22307@kagawa.kosen-ac.jp>
2025-06-02 21:56:55 +0900

    Unity6にバージョンアップ

---
+++ Assets/Projects/Scripts/ExampleFanSetter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UI.FanDebug;
+using UnityEngine;
+
+namespace Projects.Scripts
+{
+    public class ExampleFanSetter : MonoBehaviour, IFanSetter
+    {
+        [SerializeField] private float powerMultiplier = 1.0f;
+
+        private readonly Dictionary<int, ExampleForceSource> _sources = new();
+
+        private void Start()
+        {
+            foreach (var source in FindObjectsByType<ExampleForceSource>(FindObjectsSortMode.None))
+                _sources.Add(source.Index, source);
+        }
+
+        public void SetFanPower(float power, int fanIndex)
+        {
+            _sources[fanIndex].SetForce(power * powerMultiplier);
+        }
+    }
+}
---
+++ Assets/Projects/Scripts/ExampleForceSource.cs
@@ -0,0 +1,24 @@
+﻿using UniLbm.Lbm.Extension;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Projects.Scripts
+{
+    public class ExampleForceSource : MonoBehaviour, ILbmForceSource
+    {
+        [SerializeField] private int index;
+        [SerializeField] private uint3 cellSize = 1;
+
+        private float _forcePower;
+
+        public float3 Force => transform.forward * _forcePower;
+        public uint3 CellSize => cellSize;
+        public float3 Position => transform.position;
+        public int Index => index;
+
+        public void SetForce(float force)
+        {
+            _forcePower = force;
+        }
+    }
+}
